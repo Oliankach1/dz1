99a9b577fe91836dff5555576d20841179ab135e
oli <oliankach@gmail.com>
2024-01-29 00:08:03 +0300

    tretii commit

---
+++ pr.py
@@ -0,0 +1,7 @@
+a = int(input("введите сторону а >>>"))
+b = int(input("введите сторону b >>>"))
+c = int(input("введите сторону с >>>"))
+an = (input ("введите угол А >>>"))
+p = int((a+b+c)/2)
+print ("полупериметр = ", p)
+print ('nadeyus rabotae')
